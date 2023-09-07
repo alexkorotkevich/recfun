83d4bea1858d81298bde9df81b6f54e43b98ac99
Aliaksei Karatkevich <abk@krononsoft.com>
2023-09-07 17:25:41 +0300

    brackets + comments + if-then

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -1,5 +1,7 @@
 package recfun
 
+import scala.annotation.tailrec
+
 object RecFun extends RecFunInterface:
 
   def main(args: Array[String]): Unit =
@@ -9,35 +11,28 @@ object RecFun extends RecFunInterface:
         print(s"${pascal(col, row)} ")
       println()
 
-  def pascal(c: Int, r: Int): Int = {
-    if (c == r || (c == 0)) 1
+  def pascal(c: Int, r: Int): Int =
+    if c == r || (c == 0) then 1
     else
       pascal(c - 1, r - 1) + pascal(c, r - 1)
-  }
 
   /**
    * Exercise 2
    */
-//  def balance(chars: String): Boolean = {
-    def balance(chars: List[Char]): Boolean = {
-//    def chatsToList = chars.toList
-    def balanced(chars: List[Char], open: Int): Boolean = {
-      if (chars.isEmpty) open == 0
-      else if (chars.head == '(') balanced(chars.tail, open + 1)
-      else if (chars.head == ')') open > 0 && balanced(chars.tail, open - 1)
+  def balance(chars: List[Char]): Boolean =
+    def balanced(chars: List[Char], open: Int): Boolean =
+      if (chars.isEmpty) then open == 0
+      else if (chars.head == '(') then balanced(chars.tail, open + 1)
+      else if (chars.head == ')') then open > 0 && balanced(chars.tail, open - 1)
       else balanced(chars.tail, open)
-    }
     balanced(chars, 0)
-  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-    def calc(money: Int, coins: List[Int]): Int = {
+  def countChange(money: Int, coins: List[Int]): Int =
+    def calc(money: Int, coins: List[Int]): Int =
       if (coins.isEmpty || money < 0) 0
       else if (money == 0) 1
       else calc(money, coins.tail) + calc(money - coins.head, coins)
-    }
     calc(money, coins)
-  }
