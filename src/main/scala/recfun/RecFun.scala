e63e3559501699a6d7d43831d25b2aed53fa8ce9
Aliaksei Karatkevich <abk@krononsoft.com>
2023-09-08 09:12:18 +0300

    countChange-function-without-calc-function

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -31,9 +31,8 @@ object RecFun extends RecFunInterface:
   /**
    * Exercise 3
    */
+
   def countChange(money: Int, coins: List[Int]): Int =
-    def calc(money: Int, coins: List[Int]): Int =
-      if (coins.isEmpty || money < 0) 0
-      else if (money == 0) 1
-      else calc(money, coins.tail) + calc(money - coins.head, coins)
-    calc(money, coins)
+      if (coins.isEmpty || money < 0) then 0
+      else if (money == 0) then 1
+      else countChange(money, coins.tail) + countChange(money - coins.head, coins)
